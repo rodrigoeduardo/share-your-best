33c95845df6c7d84c10b09b757d56d455700b5d0
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-10-14 20:32:11 -0300

    creating first page

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <Flex flexDir="column" maxW="1080px" margin="0 auto">
+      {/* LOGO */}
+      <Text fontSize="3rem" fontWeight="700" textAlign="center">
+        share your
+        <Text as="span" ml="0.8rem" color="green.600">
+          best
+        </Text>
+        <Text as="span" ml="0.2rem">
+          !
+        </Text>
+      </Text>
+      {/* LOGO */}
+
+      <Text fontSize="0.8rem" textAlign="center">
+        Login in with Spotify and share your most listened artists, albums and
+        playlists.
+      </Text>
+    </Flex>
+  );
+}
