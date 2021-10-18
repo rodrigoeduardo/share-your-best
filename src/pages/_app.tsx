d873a702cdb978a75b21268c9ed84fda7d35c7db
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-10-18 00:04:42 -0300

    NextAuthProvider added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
+import { AppProps } from 'next/app';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
+import { Provider as NextAuthProvider } from 'next-auth/client';
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <NextAuthProvider session={pageProps.session}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </NextAuthProvider>
   );
 }
 
