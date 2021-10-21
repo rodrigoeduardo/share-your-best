7e71b999ece72f7d2f89c6f554b848e82b847de0
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-10-20 21:02:39 -0300

    Logo component updated

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { Text } from '@chakra-ui/react';
 
 export function Logo() {
   return (
-    <Text fontSize="3rem" fontWeight="700" textAlign="center">
+    <Text fontSize="6rem" fontWeight="700">
       share your
       <Text as="span" ml="0.8rem" color="green.600">
         best
