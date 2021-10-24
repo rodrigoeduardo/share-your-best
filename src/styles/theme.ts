ab32191737bf88f48d7085998ab2324dd518e5bc
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-10-24 00:44:22 -0300

    chakra breakpoints created

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+  sm: '300px',
+  md: '900px',
+  lg: '1080px',
+  xl: '1300px',
+});
 
 export const theme = extendTheme({
+  breakpoints,
   colors: {
     gray: {
       500: '#181818',
@@ -15,6 +24,14 @@ export const theme = extendTheme({
   },
   styles: {
     global: {
+      html: {
+        fontSize: {
+          base: '62.5%',
+          md: '75%',
+          xl: '100%',
+        },
+        overflowX: 'hidden',
+      },
       body: {
         bg: 'gray.500',
         color: 'white',
